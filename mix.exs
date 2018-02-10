a10ee0b3ea528ecdd28c7d54526246a04fbc6688
Wouter Roosendaal <tuvokki@gmail.com>
2018-02-10 08:34:06 +0100

    Update

--- mix.exs
+++ mix.exs
@@ -6,11 +6,21 @@ defmodule Gesture.Mixfile do
       app: :gesture,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      # Docs
+      name: "Gesture drawing",
+      source_url: "https://github.com/tuvokki/gesture",
+      homepage_url: "http://www.tuvok.nl",
+      # The main page in the docs
+      docs: [
+        main: "GestureDrawingApp",
+        logo: "assets/static/images/gesture_drawing_logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,7 +36,7 @@ defmodule Gesture.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -40,7 +50,8 @@ defmodule Gesture.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
 
@@ -54,7 +65,7 @@ defmodule Gesture.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
